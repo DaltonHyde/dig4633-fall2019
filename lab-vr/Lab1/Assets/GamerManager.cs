261564285a05f378d8be6df691f7483468eab6e1
Dalton Hyde <daltonhyde14@gmail.com>
2019-09-25 17:31:56 -0400

    the lab?
    works??

---
+++ lab-vr/Lab1/Assets/GamerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamerManager : MonoBehaviour
+{
+    public bool hatRestored = false;
+
+    public bool armRestored = false;
+
+    public GameObject finishText;
+
+
+    public void checkForVictory()
+    {
+        if (hatRestored && armRestored )
+        {
+          Debug.Log("Dalton is perplexed");
+            finishText.SetActive(true); 
+        }
+    }
+    public void hatWasRestored()
+    {
+        this.hatRestored = true;
+        checkForVictory();
+           
+    }
+
+    public void armWasRestored()
+    {
+        this.armRestored = true;
+        checkForVictory();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
